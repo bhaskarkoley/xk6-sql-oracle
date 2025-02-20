d7138921d0f7c5c63a506f869cb8513f2bdebcd1
marek-wrobel <71870617+marek-wrobel@users.noreply.github.com>
2025-02-20 14:37:02 +0000

    replacing old testutils approach

--- sql_test.go
+++ sql_test.go
@@ -5,19 +5,36 @@ import (
 	"io"
 	"testing"
 
-	"github.com/spf13/afero"
-
 	"github.com/grafana/sobek"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modulestest"
 	"go.k6.io/k6/lib"
-	"go.k6.io/k6/lib/testutils"
 	"go.k6.io/k6/metrics"
 )
 
-// TestSQLiteIntegration performs an integration test creating an SQLite database
+// SimpleLogrusHook is a minimal replacement for testutils.SimpleLogrusHook.
+// It implements logrus.Hook and is configured to trigger on the specified log levels.
+type SimpleLogrusHook struct {
+	HookedLevels []logrus.Level
+}
+
+// Levels returns the log levels that this hook should be triggered for.
+func (hook *SimpleLogrusHook) Levels() []logrus.Level {
+	return hook.HookedLevels
+}
+
+// Fire is called when a log entry with one of the hooked levels is logged.
+// For testing purposes, this example does nothing.
+// You can add additional logic (such as capturing the log entry) if needed.
+func (hook *SimpleLogrusHook) Fire(entry *logrus.Entry) error {
+	// No-op: this hook intentionally does nothing.
+	return nil
+}
+
+// TestSQLiteIntegration performs an integration test creating an SQLite database.
 func TestSQLiteIntegration(t *testing.T) {
 	t.Parallel()
 
@@ -35,14 +52,14 @@ func TestSQLiteIntegration(t *testing.T) {
 	_, err := rt.RunString(`
 const db = sql.open("sqlite3", "` + dbname + `");
 
-db.exec("DROP TABLE IF EXISTS test_table;")
-db.exec("CREATE TABLE test_table (id integer PRIMARY KEY AUTOINCREMENT, key varchar NOT NULL, value varchar);")
+db.exec("DROP TABLE IF EXISTS test_table;");
+db.exec("CREATE TABLE test_table (id integer PRIMARY KEY AUTOINCREMENT, key varchar NOT NULL, value varchar);");
 
 for (let i = 0; i < 5; i++) {
-	db.exec("INSERT INTO test_table (key, value) VALUES ('key-" + i + "', 'value-" + i + "');")
+	db.exec("INSERT INTO test_table (key, value) VALUES ('key-" + i + "', 'value-" + i + "');");
 }
 
-let all_rows = sql.query(db, "SELECT * FROM test_table;")
+let all_rows = sql.query(db, "SELECT * FROM test_table;");
 if (all_rows.length != 5) {
 	throw new Error("Expected all five rows to be returned; got " + all_rows.length)
 }
@@ -57,7 +74,7 @@ if (no_rows.length != 0) {
 	throw new Error("Expected no rows to be returned; got " + no_rows.length)
 }
 
-db.close()
+db.close();
 `)
 	require.NoError(t, err)
 }
@@ -67,7 +84,8 @@ func setupTestEnv(t *testing.T) *sobek.Runtime {
 	rt.SetFieldNameMapper(common.FieldNameMapper{})
 
 	testLog := logrus.New()
-	testLog.AddHook(&testutils.SimpleLogrusHook{
+	// Add our custom simple hook to capture WarnLevel logs.
+	testLog.AddHook(&SimpleLogrusHook{
 		HookedLevels: []logrus.Level{logrus.WarnLevel},
 	})
 	testLog.SetOutput(io.Discard)
